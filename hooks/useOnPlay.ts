18205b5e20dd95874afaf70ee84b8f0225629c42
maxyeh001 <52844708+maxyeh001@users.noreply.github.com>
2025-11-09 22:58:32 -0800

    Update useOnPlay.ts

--- hooks/useOnPlay.ts
+++ hooks/useOnPlay.ts
@@ -11,13 +11,13 @@ export const useOnPlay = (songs: Song[]) => {
   const { user, subscription } = useUser();
 
   const usePlay = (id: string) => {
-    if (!user) {
-      return authModal.onOpen();
-    }
+    //if (!user) {
+      //return authModal.onOpen();
+    //}
 
-    if (!subscription) {
-      return subscribeModal.onOpen();
-    }
+    //if (!subscription) {
+      //return subscribeModal.onOpen();
+    //}
 
     player.setId(id);
     player.setIds(songs.map((song) => song.id));
