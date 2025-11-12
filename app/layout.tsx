a865097353b70a5833275266e20f2277bf5ec1e5
maxyeh001 <52844708+maxyeh001@users.noreply.github.com>
2025-11-11 23:11:32 -0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,8 +19,8 @@ const font = Figtree({ subsets: ['latin'] });
 
 //* Describe the web app
 export const metadata = {
-  title: 'Spotify Clone',
-  description: 'Listen to music!',
+  title: 'Boyfriend ASMR',
+  description: 'Listen to your favorite ASMRtists!',
 };
 
 export const revalidate = 0;
