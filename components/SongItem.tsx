04ecf0d10d4a0c7cd17d8ab5077d20c33dcf85f2
maxyeh001 <52844708+maxyeh001@users.noreply.github.com>
2025-12-13 08:44:31 -0800

    Update SongItem.tsx

--- components/SongItem.tsx
+++ components/SongItem.tsx
@@ -36,9 +36,9 @@ export const SongItem: React.FC<SongItemProps> = ({ data, onClick }) => {
         <Image loading="eager" className="object-cover" src={imagePath || '/images/liked.png'} fill alt="Image" />
       </div>
 
-      <div className="flex flex-col items-start w-full pt-4 gap-y-1">
+      <div className="flex flex-col items-start w-full pt-3 gap-y-1">
         <p className="font-semibold truncate w-full">{data.title}</p>
-        <p className="text-neutral-400 text-sm pb-4 w-full truncate">
+        <p className="text-neutral-400 text-sm pb-2 w-full truncate">
           By{' '}
           {data.artist_id ? (
             <Link
