40017d93c68d6add7a4cffe1fe350a21400b1712
maxyeh001 <52844708+maxyeh001@users.noreply.github.com>
2025-12-13 09:25:01 -0800

    Update page.tsx

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -21,10 +21,8 @@ export default async function Home() {
   return (
     <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
 
-      {/* HEADER */}
-      <Header>
-        <h1 className="text-white text-3xl font-bold mb-4">Welcome back</h1>
-      </Header>
+    <Header />
+
 
       {/* TRENDING SONGS */}
       <section className="px-6 mb-8">
