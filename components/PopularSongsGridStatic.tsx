ca5fc67ebfe25a9a7a1482c9b134f089c28a7cf0
maxyeh001 <52844708+maxyeh001@users.noreply.github.com>
2025-12-15 18:34:37 -0800

    Update PopularSongsGridStatic.tsx

--- components/PopularSongsGridStatic.tsx
+++ components/PopularSongsGridStatic.tsx
@@ -15,17 +15,30 @@ export default function PopularSongsGridStatic({ songs }: Props) {
   return (
     <div className="mt-4">
       {/* SONG GRID */}
-      <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4">
+      <div
+        className="
+          grid
+          grid-cols-2
+          sm:grid-cols-3
+          md:grid-cols-4
+          lg:grid-cols-6
+          gap-4
+        "
+      >
         {songs.map((song) => (
-          <SongItem
+          <div
             key={song.id}
-            data={song}
-            onClick={(id) => onPlay(id)}
-          />
+            className="max-w-[180px] w-full justify-self-start"
+          >
+            <SongItem
+              data={song}
+              onClick={(id) => onPlay(id)}
+            />
+          </div>
         ))}
       </div>
 
-      {/* SHOW ALL BUTTON (centered) */}
+      {/* SHOW ALL BUTTON */}
       <div className="flex justify-center mt-8">
         <Link
           href="/songs/popular"
